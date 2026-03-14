f6d9f560cfd28231c4c590ed0e00940f8f4cf7d7
Eddy <eddy@example.com>
2026-03-13 23:40:35 -0500

    re-did everything

--- scripts/petsProfile.js
+++ scripts/petsProfile.js
@@ -1,4 +1,30 @@
-//this gives us the ability to switch between pictures
-function changeImage(element) {
-    document.getElementById("mainImage").src = element.src;
+let images = [
+    "/images/dog.jpeg",
+    "/images/dog2.jpeg",
+];
+
+let index = 0;
+
+function showImage(){
+    document.getElementById("productImage").src = images[index];
+}
+
+function nextImage(){
+    index++;
+
+    if(index >= images.length){
+        index = 0;
+    }
+
+    showImage();
+}
+
+function prevImage(){
+    index--;
+
+    if(index < 0){
+        index = images.length - 1;
+    }
+
+    showImage();
 }
